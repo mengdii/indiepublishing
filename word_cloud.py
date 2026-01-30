fdc7e046b017e3f7aba482ab0617689e4d42fcf9
Mengdi Zhang <44301078+mengdii@users.noreply.github.com>
2026-01-30 07:13:40 +0200

    Update word_cloud.py

--- word_cloud.py
+++ word_cloud.py
@@ -1,15 +1,14 @@
 
-
 ! pip install wordcloud pandas matplotlib
 
 import pandas as pd
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-# Load the CSV file
+# Load file (top 50 frequent words of each region)
 df = pd.read_csv("frequency-global south.csv")
 
-# Create a dictionary: {word: frequency}
+# Create a dictionary
 word_freq = dict(zip(df["word"], df["frequency"]))
 
 # Generate the word cloud
@@ -19,7 +18,7 @@ wc = WordCloud(
     background_color="white"
 ).generate_from_frequencies(word_freq)
 
-# Display the word cloud
+# Display
 plt.figure(figsize=(12, 8))
 plt.imshow(wc, interpolation="bilinear")
 plt.axis("off")
